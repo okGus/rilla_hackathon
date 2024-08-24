30679b7f8c7684ee13c27296262fd1c270226f09
Gustavo <rosalesagustavo@gmail.com>
2024-08-24 16:06:08 -0400

    signout functionality from clerk

--- src/app/component/HomePage.tsx
+++ src/app/component/HomePage.tsx
@@ -1,20 +1,40 @@
 import React from 'react';
-import { Button, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useRouter } from "next/navigation";
+import { UserButton } from '@clerk/nextjs';
 
 
 const HomePage: React.FC = () => {
   const router = useRouter();
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '20vh' }}>
-      <Typography variant="h3" component="h1" gutterBottom>
-        Rilla
-      </Typography>
-      <Button onClick={() => router.push('/create-transcript')} variant="contained" color="primary" style={{ marginTop: '20px' }}>
-        Create Transcript
-      </Button>
-    </div>
+    <Box>
+      <Box
+        display={'flex'}
+        height={'70px'}
+        justifyContent={'space-between'}
+        padding={'20px'}
+        sx={{ width: '100%' }}
+      >   
+        <Typography 
+          variant={"h6"}
+          color={'grey'}
+        >
+          Rilla
+        </Typography>
+        <UserButton 
+          showName 
+        />
+      </Box>
+      <div style={{ textAlign: 'center', marginTop: '20vh' }}>
+        <Typography variant="h3" component="h1" gutterBottom>
+          Rilla
+        </Typography>
+        <Button onClick={() => router.push('/create-transcript')} variant="contained" color="primary" style={{ marginTop: '20px' }}>
+          Create Transcript
+        </Button>
+      </div>
+    </Box>
   );
 };
 
